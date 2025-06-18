95ca1c6a1ea1ef99615d2ff2ac3774affbdd4ba8
landwarrior <tech_land_equilibrium@outlook.jp>
2025-06-19 00:12:18 +0900

    チュートリアルを見ながらメソッドを追加してみる

--- MyMvcApp/Program.cs
+++ MyMvcApp/Program.cs
@@ -35,6 +35,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}/{id2?}");
 
 app.Run();
